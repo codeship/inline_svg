9aceb803f43c1b9bade12b2ebc29e27becc50e38
Benjamin Fritsch <beanie@benle.de>
2016-01-23 11:22:52 -0500

    Update railtie.rb

--- lib/inline_svg/railtie.rb
+++ lib/inline_svg/railtie.rb
@@ -1,18 +1,18 @@
-require 'rails/railtie'
+require('rails/railtie')
+require_relative('static_asset_finder')
+
 module InlineSvg
   class Railtie < ::Rails::Railtie
-    initializer "inline_svg.action_view" do |app|
-      ActiveSupport.on_load :action_view do
-        require "inline_svg/action_view/helpers"
-        include InlineSvg::ActionView::Helpers
+    initializer("inline_svg.action_view") do |app|
+      ActiveSupport.on_load(:action_view) do
+        require("inline_svg/action_view/helpers")
+        include(InlineSvg::ActionView::Helpers)
       end
     end
 
     config.after_initialize do |app|
       InlineSvg.configure do |config|
-        # In default Rails apps, this will be a fully operational
-        # Sprockets::Environment instance
-        config.asset_finder = app.instance_variable_get(:@assets)
+        config.asset_finder = StaticAssetFinder
       end
     end
   end
